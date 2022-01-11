27ba6e625eb899bcb25c5be63ff36c390c1a1c8d
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2022-01-11 07:21:53 +0100

    Hello

--- src/be/vdab/hello/HelloWorld.java
+++ src/be/vdab/hello/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Belgium!");
+        System.out.println("Hello Java!");
     }
 }
