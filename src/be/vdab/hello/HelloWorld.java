cb5dd33dba7e7d7db8a422df1a1ce4280e83ec33
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2022-01-12 07:24:32 +0100

    Variabelen

--- src/be/vdab/hello/HelloWorld.java
+++ src/be/vdab/hello/HelloWorld.java
@@ -6,5 +6,17 @@ public class HelloWorld {
         System.out.println("Hello Belgium!");
         System.out.println("Hello Java!");
         System.out.println("Hello Java!");
+
+        int bedrag = 100;
+        byte aantalKinderen = 2;
+        double salaris = 1525.50;
+        boolean gehuwd = true;
+        char letter = 'a';
+        String voornaam = "Jan";
+        String zin = "Hij zei: \" Hier volgt een \nnieuwe lijn.\"";
+        System.out.println(zin);
+
+        final int AANTAL_PROVINCIES = 10;
+        final float PI = 3.141592F;
     }
 }
