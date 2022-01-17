4709eb34f3edc6182e8b9a7c0ee540926a0f1b4a
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2022-01-17 07:58:02 +0100

    operatoren

---
+++ src/be/vdab/operatoren/Bmi.java
@@ -0,0 +1,11 @@
+package be.vdab.operatoren;
+
+public class Bmi {
+    public static void main(String[] args) {
+        float gewicht = 75.7F;
+        float lengte = 1.70F;
+        float bmi;
+        bmi = gewicht / (lengte * lengte);
+        System.out.println("De bodymassindex is " + bmi);
+    }
+}
