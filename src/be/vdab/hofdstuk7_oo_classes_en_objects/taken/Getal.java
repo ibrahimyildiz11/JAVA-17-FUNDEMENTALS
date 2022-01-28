f8ff1fe4885bab711326b05e788496c18fdd0b1b
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2022-01-28 16:57:02 +0100

    getal taak

--- src/be/vdab/hofdstuk7_oo_classes_en_objects/taken/Getal.java
+++ src/be/vdab/hofdstuk7_oo_classes_en_objects/taken/Getal.java
@@ -4,14 +4,42 @@ public class Getal {
     private int x;
 
     public Getal(int a) {
-        x = a;
+       setX(a);
     }
 
     public void print() {
-        System.out.println(x);
+        System.out.println("X = " + x);
     }
 
     public int absoluut() {
-        return (int) Math.abs(x);
+        return Math.abs(x);
+    }
+
+    public int som(int a) {
+        return x+a;
+    }
+
+    public float som(float a) {
+        return x+a;
+    }
+
+    public double som(double a) {
+        return x+a;
+    }
+
+    public void add(int a) {
+        x+= a;
+    }
+
+    public double toDouble() {
+        return (double) x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int a) {
+        x = a;
     }
 }
--- src/be/vdab/hofdstuk7_oo_classes_en_objects/taken/GetalMain.java
+++ src/be/vdab/hofdstuk7_oo_classes_en_objects/taken/GetalMain.java
@@ -2,8 +2,24 @@ package be.vdab.hofdstuk7_oo_classes_en_objects.taken;
 
 public class GetalMain {
     public static void main(String[] args) {
-        var getal1 = new Getal(-5);
+        var getal1 = new Getal(-45);
         getal1.print();
         System.out.println(getal1.absoluut());
+        System.out.println(getal1.som(6));
+
+        var getal2 = new Getal(10);
+        System.out.println("GETAL2: ");
+        System.out.println("\t" + getal2.absoluut());
+        getal2.add(5);
+        System.out.print("\t");
+        getal2.print();
+
+        System.out.println(getal2.toDouble());
+        getal2.print();
+
+        getal2.setX(99);
+        getal2.print();
+        var xx = getal2.getX();
+        System.out.println("getX geeft :" + xx);
     }
 }
